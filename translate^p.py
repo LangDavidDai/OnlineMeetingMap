a2bd92dc037afb2c9fa91f49fa8dbc90746c36bf
LangDavidDai <76823758+LangDavidDai@users.noreply.github.com>
2022-02-22 18:56:48 +0800

    20220222 map update

---
+++ translate^p.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import csv
+
+with open('before.csv','r') as f:
+    reader=csv.reader(f)
+    fo=open('after.txt','w+')
+    for i in reader:
+        fo.write(i[0]+',')
+    fo.close()
+print('---------------------------------完成------------------------------')
---
+++ tsx/fantasy 02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.1" name="fantasy 02" tilewidth="32" tileheight="32" tilecount="240" columns="8">
+ <image source="../png/fantasy 02.png" width="256" height="960"/>
+</tileset>
